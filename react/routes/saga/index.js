3611ea145d64c65c46b9d468af3813ed2c39dfe7
20399 <yanfang.lin@hand-china.com>
2021-03-25 14:20:53 +0800

    [IMP] 添加Route权限

--- react/routes/saga/index.js
+++ react/routes/saga/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import { asyncRouter, nomatch } from '@choerodon/boot';
+import { PermissionRoute } from '@choerodon/master';
 
 const index = asyncRouter(() => import('./Saga'));
 
 const Index = ({ match }) => (
   <Switch>
-    <Route exact path={match.url} component={index} />
+    <PermissionRoute
+      exact
+      path={match.url}
+      component={index}
+      service={['choerodon.code.site.manager.saga-manager.saga.ps.default']}
+    />
     <Route path="*" component={nomatch} />
   </Switch>
 );
--- react/routes/task-detail/index.js
+++ react/routes/task-detail/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import { asyncRouter, nomatch } from '@choerodon/boot';
+import { PermissionRoute } from '@choerodon/master';
 
 const index = asyncRouter(() => (import('./List')));
 
 const Index = ({ match }) => (
   <Switch>
-    <Route exact path={match.url} component={index} />
+    <PermissionRoute
+      exact
+      path={match.url}
+      component={index}
+      service={(type) => (type === 'organization'
+        ? ['choerodon.code.organization.manager.task.ps.default']
+        : ['choerodon.code.site.manager.task-detail.ps.default'])}
+    />
     <Route path="*" component={nomatch} />
   </Switch>
 );
